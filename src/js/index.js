a867abb37b113e2f9dd451569416b9c9e585cb88
msko <kms89123@gmail.com>
2021-07-14 23:12:31 +0900

    Update Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import '../scss/common.scss';
+// import logo from '../assets/logo.png';
 
 // console.log('index');
+
+// const logoEl = document.querySelector('img');
+// logoEl.src = logo;
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ const HtmlPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/js/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
     clean: true, // 재빌드 시, 필요없는 파일들 제거
   },
   module: {
@@ -18,18 +19,34 @@ module.exports = {
         test: /\.js$/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        type: 'asset/resource',
+      },
+      // {
+      //   test: /\.html$/i,
+      //   loader: 'html-loader',
+      // },
+      // {
+      //   test: /\.html/,
+      //   type: 'asset/resource',
+      //   generator: {
+      //     filename: 'static/[hash][ext][query]',
+      //   },
+      // },
     ],
   },
   resolve: {
     extensions: ['.wasm', '.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '~': path.resolve(__dirname, 'src'),
+      assets: path.resolve(__dirname, 'src/assets'),
     },
   },
   plugins: [
     new HtmlPlugin({
       template: './src/html/index.html',
-      favicon: './favicon.ico',
+      favicon: './src/assets/favicon.ico',
     }),
   ],
   devServer: {
